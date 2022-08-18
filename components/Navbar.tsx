b7ccdc1a8700a4bde343742f0546a2460686e413
Michał <michal.3zalewski@icloud.com>
2022-08-18 16:58:10 +0200

    Zustand and GoogleAuth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import Image from "next/Image";
 import Link from "next/link";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
+
 import { useRouter } from "next/router";
-import { GoogleLogin, GoogleLogout } from "react-google-login";
+// import { GoogleLogin, GoogleLogout } from "react-google-login";
 import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
 
 import Logo from "../utils/tiktik-logo.png";
+import { createOrGetUser } from "../utils";
+
+import useAuthStore from '../store/authStore'
 
 const Navbar = () => {
+	const { userProfile, addUser } = useAuthStore()
+
 	return (
-		<div className="w-full flex justify-between items-center 
-      border-b-2 border-gray-200 py-2">
+		<div
+			className="w-full flex justify-between items-center 
+      border-b-2 border-gray-200 p-2"
+		>
 			<Link href="/">
 				<div className="w-[100px] md:w-[130px] ">
 					<Image
@@ -20,9 +29,22 @@ const Navbar = () => {
 						src={Logo}
 						alt="TikTik"
 						layout="responsive"
+						priority={true}
 					/>
 				</div>
 			</Link>
+			<div>Search</div>
+			<div>
+				{userProfile ? (
+					<div>{userProfile?.userName}</div>
+				) : (
+					<GoogleLogin
+						onSuccess={(response) => {
+							createOrGetUser(response, addUser)
+						}}
+					/>
+				)}
+			</div>
 		</div>
 	);
 };
--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,53 +1,114 @@
-import { Video } from '../types'
-import { NextPage} from 'next'
-import React, { useEffect, useRef, useState } from 'react'
-import Image from 'next/Image';
-import Link from 'next/Link';
-
-import { HiVolumeUp, HiVolumeOff} from 'react-icons/hi'
-import { BsFillPlayFill, BsFillPauseFill} from 'react-icons/bs'
-import { GoVerified } from 'react-icons/go'
+import { Video } from "../types";
+import { NextPage } from "next";
+import React, { useEffect, useRef, useState } from "react";
+import Image from "next/Image";
+import Link from "next/Link";
+import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
+import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
+import { GoVerified } from "react-icons/go";
 
 interface IProps {
-  post: Video
+	post: Video;
 }
 
-const VideoCard: NextPage<IProps> = ({post}) => {
-  
-  
-  return (
-    <div className="flex flex-col border-b-2 border-gray-200 pb-6">
-      <div>
-        <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
-          <div className="md:w-16 md:h-16 w-10 h-10">
-            <Link href="/">
-              <>
-                <Image 
-                  width={62}
-                  height={62}
-                  className="rounded-full"
-                  src={post.postedBy.image}
-                  alt="profile photo"
-                  layout="responsive"
-                />
-              </>
-            </Link>
-          </div>
-          <div>
-            <Link href="/">
-              <div>
-                <p>
-                  {post.postedBy.userName}{' '}
-                  <GoVerified className="text-blue-400 text-md"/>
-                </p>
-                <p>{post.postedBy.userName}</p>
-              </div>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+const VideoCard: NextPage<IProps> = ({ post }) => {
+	const [isHover, setIsHover] = useState(false);
+	const [isPlaying, setIsPlaying] = useState(false);
+	const [isVideoMuted, setIsVideoMuted] = useState(false);
 
-export default VideoCard
+  const videoRef = useRef<HTMLVideoElement>(null)
+
+  const onVideoPress = () => {
+    if (isPlaying) {
+      videoRef?.current?.pause()
+      setIsPlaying(false)
+    } else {
+      videoRef?.current?.play()
+      setIsPlaying(true)
+    }
+  }
+
+  const videoIconClass = "text-white text-2xl lg:text-4xl"
+
+	return (
+		<div className="flex flex-col border-b-2 border-gray-200 pb-6">
+			<div>
+				<div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+					<div className="md:w-16 md:h-16 w-10 h-10">
+						<Link href="/">
+							<>
+								<Image
+									width={62}
+									height={62}
+									className="rounded-full"
+									src={post.postedBy.image}
+									alt="profile photo"
+									layout="responsive"
+								/>
+							</>
+						</Link>
+					</div>
+					<div>
+						<Link href="/">
+							<div>
+								<p>
+									{post.postedBy.userName}{" "}
+									<GoVerified className="text-blue-400 text-md" />
+								</p>
+								<p className="capitalize font-medium text-xs text-gray-500 hidden md:block">
+									{post.postedBy.userName}
+								</p>
+							</div>
+						</Link>
+					</div>
+				</div>
+			</div>
+			<div className="lg:ml-20 w-[70vw] xl:w-[50vw] flex gap-4 relative shadow-2xl">
+				<div
+					className="rounded-3xl"
+					onMouseEnter={() => {
+						setIsHover(true);
+					}}
+					onMouseLeave={() => {
+						setIsHover(false);
+					}}
+				>
+					<Link href=".">
+						<video
+							src={post.video.asset.url}
+							loop
+              muted={!!isVideoMuted}
+              ref={videoRef}
+							className="object-cover  w-full  rounded-2xl cursor-pointer bg-gray-100 brightness-100"
+						></video>
+					</Link>
+
+					{isHover && (
+						<div className="absolute z-100 bottom-10 cursor-pointer w-full flex gap-10 justify-between px-20 ">
+							{isPlaying ? (
+								<button onClick={onVideoPress}>
+                    <BsFillPauseFill className={videoIconClass}/>
+								</button>
+							) : (
+								<button onClick={onVideoPress}>
+                    <BsFillPlayFill className={videoIconClass}/>
+								</button>
+							)}
+							{isVideoMuted ? (
+								<button onClick={() => setIsVideoMuted(false)}>
+                    <HiVolumeOff className={videoIconClass}/>
+								</button>
+							) : (
+								<button onClick={() => setIsVideoMuted(true)}>
+                    <HiVolumeUp className={videoIconClass}/>
+								</button>
+							)}
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default VideoCard;
